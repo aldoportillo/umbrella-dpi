2fc3817d3f7916ae9945620697c59d42b82a0f67
Aldo Portillo <94483081+aldoportillo@users.noreply.github.com>
2023-07-21 18:03:33 +0000

    Get current temp

--- main.rb
+++ main.rb
@@ -4,20 +4,27 @@ require "json"
 
 #Get Location
 pp "What's your location? "
-location = "chicago"
+location = gets.chomp
 
-#Get Coordinates
+#Get Coordinates using Maps API
 
-URI = "https://maps.googleapis.com/maps/api/geocode/json?address=#{location}&key=#{ENV.fetch("GMAPS_KEY")}"
+GMAPS_URI = "https://maps.googleapis.com/maps/api/geocode/json?address=#{location}&key=#{ENV.fetch("GMAPS_KEY")}"
 
-req = HTTP.get(URI)
+gmaps_req = HTTP.get(GMAPS_URI)
 
-res = JSON.parse(req)
+gmaps_res = JSON.parse(gmaps_req)
 
-lat = res.fetch("results").at(0).fetch("geometry").fetch("location").fetch("lat")
-lng = res.fetch("results").at(0).fetch("geometry").fetch("location").fetch("lng")
+lat = gmaps_res.fetch("results").at(0).fetch("geometry").fetch("location").fetch("lat")
+lng = gmaps_res.fetch("results").at(0).fetch("geometry").fetch("location").fetch("lng")
  
-pp lat
-pp lng
+## Get weather using Pirate API
+PIRATE_URI = "https://api.pirateweather.net/forecast/#{ENV.fetch("PIRATE_WEATHER_KEY")}/#{lat},#{lng}" #Might need to swap coordinates
 
-pp location
+pirate_req = HTTP.get(PIRATE_URI) ##If I were to further this app with a UI, I would write a function for these two to modulate the app...not dry at all
+pirate_res = JSON.parse(pirate_req) 
+
+
+
+current_temp = pirate_res.fetch("currently").fetch("temperature")
+
+pp "It is currently #{current_temp} F"
